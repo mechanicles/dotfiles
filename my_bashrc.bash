12f8d48772ee8ac12a300f0d0096eb38ef345d87
Santosh Wadghule <santosh.wadghule@gmail.com>
2013-08-24 23:55:40 +0530

    Update submodules and some small changes

---
+++ my_bashrc.bash
@@ -0,0 +1,47 @@
+# Commands
+alias solr_start='rake sunspot:solr:start'
+alias solr_stop='rake sunspot:solr:stop'
+alias test_solr_start='RAILS_ENV=test rake sunspot:solr:start'
+alias test_solr_stop='RAILS_ENV=test rake sunspot:solr:stop'
+alias dj_start='script/delayed_job start'
+alias dj_stop='script/delayed_job stop'
+alias tlf="tail -f"
+alias l='ls'
+alias ll='ls -al'
+alias ls='ls -ltrG'
+alias lh='ls -Alh'
+alias paux='ps aux| grep'
+
+# Bundler
+alias b="bundle"
+
+# Rubygems
+alias gi="gem install"
+alias giv="gem install -v"
+
+# Git
+alias gl='git pull --prune'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gp='git push origin HEAD'
+alias gd='git diff'
+alias gc='git commit'
+alias gca='git commit -a'
+alias gco='git checkout'
+alias gb='git branch'
+alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gsf='git submodule foreach git pull' # update submodules
+
+
+# Tmux
+alias tmux="TERM=screen-256color-bce tmux"
+alias tpob="tmux new -s opbuilder"
+
+# Show git branch name to your shell prompt
+function parse_git_branch_and_add_brackets {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\ \[\1\]/'
+}
+PS1="\h:\W \u\[\033[0;32m\]\$(parse_git_branch_and_add_brackets) \[\033[0m\]\$ "
+
+
+# Your local/machine dependent bash scripts
+[[ -s ${HOME}/.local_bashrc.bash ]] && source ${HOME}/.local_bashrc.bash
