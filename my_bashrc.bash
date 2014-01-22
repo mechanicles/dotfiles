c7601e822cba39de5c398c0d28d8107f00cb013f
Santosh Wadghule <santosh.wadghule@gmail.com>
2014-01-22 23:20:24 +0530

    Update submoduels

--- my_bashrc.bash
+++ my_bashrc.bash
@@ -21,6 +21,12 @@ alias be="bundle exec"
 alias gi="gem install"
 alias giv="gem install -v"
 
+# Filesystem
+alias ..="cd ../"
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias dotf="cd $HOME/dotfiles"
+
 # Git
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
@@ -32,7 +38,6 @@ alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gsf='git submodule foreach git pull' # update submodules
-
 
 # Tmux
 alias tmux="TERM=screen-256color-bce tmux"
