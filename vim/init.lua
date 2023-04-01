a4b19037af41e9a39f8962c9f1e63a497d8481d6
Santosh Wadghule <santosh.wadghule@gmail.com>
2023-04-01 16:58:20 +0530

    Exit from terminal setting

--- vim/init.lua
+++ vim/init.lua
@@ -283,6 +283,9 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+-- Exit from Terminal
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { noremap = true })
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
